41041055d24b4350af3ca2f8cf17af77d42cbf50
Rishit01Kundu <87909272+Rishit01Kundu@users.noreply.github.com>
2021-08-03 11:24:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var firstName = "Rishit ";
+var lastName = "Kundu";
+var fullName  = firstName.concat(lastName);
+
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+var place = " I live in Dehli ";
+console.log(place.replace("Dehli","Bangalore"));
+
+var x = 200;
+var y = 300;
+var add = x + y;
+
+console.log(add);
+
+console.log(Math.sqrt(7).toFixed(2));
+
+console.log(Math.min(748,754983));
+console.log(Math.max(734578,1234567890));
